ea39ab835502676ce60ec7432a51750e5709bc70
Bunyod Abdusaidov <bunyodev@gmail.com>
2021-03-29 09:39:16 +0500

    Modified models

--- tweet/models.py
+++ tweet/models.py
@@ -4,35 +4,37 @@ from django.contrib.auth.models import User
 
 
 class Tweet(models.Model):
-    content = models.TextField(max_length=150)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateTimeField(default=timezone.now)
+    content = models.TextField(blank=False, max_length=150)
+    author = models.ForeignKey(User, related_name='tweet_author', on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
     likes = models.IntegerField(default=0)
-    dislikes = models.IntegerField(default=0)
 
     def __str__(self):
         return self.content[:5]
 
-    @property
-    def number_of_comments(self):
-        return Comment.objects.filter(commented_to=self).count()
-
-
-class Comment(models.Model):
-    content = models.TextField(max_length=150)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    commented_to = models.ForeignKey(Tweet, on_delete=models.CASCADE)
-    date = models.DateTimeField(default=timezone.now)
-
-
-class Preference(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
-    value = models.IntegerField()
-    date = models.DateTimeField(default=timezone.now)
-
-    def __str__(self):
-        return f"{str(self.user)} : {str(self.tweet)}: {str(self.value)}"
-
     class Meta:
-        unique_together = ('user', 'tweet', 'value')
+        ordering = ['date']
+
+    # @property
+    # def number_of_comments(self):
+    #     return Comment.objects.filter(commented_to=self).count()
+
+
+# class Comment(models.Model):
+#     content = models.TextField(max_length=150)
+#     author = models.ForeignKey(User, on_delete=models.CASCADE)
+#     commented_to = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+#     date = models.DateTimeField(default=timezone.now)
+#
+#
+# class Preference(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+#     value = models.IntegerField()
+#     date = models.DateTimeField(default=timezone.now)
+#
+#     def __str__(self):
+#         return f"{str(self.user)} : {str(self.tweet)}: {str(self.value)}"
+#
+#     class Meta:
+#         unique_together = ('user', 'tweet', 'value')
